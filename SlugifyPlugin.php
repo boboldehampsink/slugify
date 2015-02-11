c519e0b1d8e2f82dc0ce51c8ef36718e72aae93c
Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
2015-02-11 16:45:45 +0100

    PSR-2 fixes

--- SlugifyPlugin.php
+++ SlugifyPlugin.php
@@ -7,26 +7,26 @@ class SlugifyPlugin extends BasePlugin
     {
         return Craft::t('Slugify Twig Extension');
     }
-    
+
     public function getVersion()
     {
         return '1.1';
     }
-    
+
     public function getDeveloper()
     {
         return 'Bob Olde Hampsink';
     }
-    
+
     public function getDeveloperUrl()
     {
         return 'http://www.itmundi.nl';
     }
-    
+
     public function addTwigExtension()
     {
         Craft::import('plugins.slugify.twigextensions.SlugifyTwigExtension');
-        
+
         return new SlugifyTwigExtension();
     }
 }
