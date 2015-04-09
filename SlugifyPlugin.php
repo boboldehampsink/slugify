812a89e833d535992c6900155231e2125bf8c176
Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
2015-04-09 08:58:43 +0200

    Added docBlocks

--- SlugifyPlugin.php
+++ SlugifyPlugin.php
@@ -1,28 +1,65 @@
 <?php
+
 namespace Craft;
 
+/**
+ * Slugify Plugin.
+ *
+ * Adds a slugify twig extension to create slugs in twig.
+ *
+ * @author    Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
+ * @copyright Copyright (c) 2015, Bob Olde Hampsink
+ * @license   MIT
+ *
+ * @link      http://github.com/boboldehampsink
+ */
 class SlugifyPlugin extends BasePlugin
 {
+    /**
+     * Get plugin name.
+     *
+     * @return string
+     */
     public function getName()
     {
         return Craft::t('Slugify Twig Extension');
     }
 
+    /**
+     * Get plugin version.
+     *
+     * @return string
+     */
     public function getVersion()
     {
         return '1.1';
     }
 
+    /**
+     * Get plugin developer.
+     *
+     * @return string
+     */
     public function getDeveloper()
     {
         return 'Bob Olde Hampsink';
     }
 
+    /**
+     * Get plugin developer url.
+     *
+     * @return string
+     */
     public function getDeveloperUrl()
     {
         return 'http://www.itmundi.nl';
     }
 
+    /**
+     * Import twig extension.
+     *
+     * @return SlugifyTwigExtension
+     */
     public function addTwigExtension()
     {
         Craft::import('plugins.slugify.twigextensions.SlugifyTwigExtension');
--- twigextensions/SlugifyTwigExtension.php
+++ twigextensions/SlugifyTwigExtension.php
@@ -1,30 +1,72 @@
 <?php
+
 namespace Craft;
 
+/**
+ * Slugify Twig Extension.
+ *
+ * Twig extension to create slugs in twig.
+ *
+ * @author    Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
+ * @copyright Copyright (c) 2015, Bob Olde Hampsink
+ * @license   MIT
+ *
+ * @link      http://github.com/boboldehampsink
+ */
 class SlugifyTwigExtension extends \Twig_Extension
 {
+    /**
+     * Environment runtime.
+     */
     protected $env;
 
+    /**
+     * Get extension name.
+     *
+     * @return string
+     */
     public function getName()
     {
         return 'Slugify Twig Extension';
     }
 
+    /**
+     * Get extension filters.
+     *
+     * @return string
+     */
     public function getFilters()
     {
         return array('slugify' => new \Twig_Filter_Method($this, 'slugify'));
     }
 
+    /**
+     * Get extension functions.
+     *
+     * @return string
+     */
     public function getFunctions()
     {
         return array('slugify' => new \Twig_Function_Method($this, 'slugify'));
     }
 
+    /**
+     * Init runtime.
+     *
+     * @param \Twig_Environment $env
+     */
     public function initRuntime(\Twig_Environment $env)
     {
         $this->env = $env;
     }
 
+    /**
+     * Slugify function.
+     *
+     * @param string $slug
+     *
+     * @return string
+     */
     public function slugify($slug)
     {
         return ElementHelper::createSlug($slug);
